afcc45cc4635d36771ee17d6dd66e28dcd89c925
mannai <>
2025-10-27 09:13:03 +0000

    ?

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -19,6 +19,8 @@ function getMethods(obj: { [x: string]: { toString: () => string; }; }) {
 
 export default function QueryProcessor(query: string): string {
   if (query.trim().length === 0) return "";
+  query = query.trim();
+  if (query[-1] === '?') query = query.slice(0, -1)
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
